583bb0edfc8f957617eec8a10831d94a4e573fbc
Ben Lobaugh <ben@lobaugh.net>
2012-05-30 13:21:18 -0700

    Creating the initial plugin files

---
+++ 2012-wordcamp-oc-wp-cron.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ Plugin Name: 2012 WordCamp Orange County WP-Cron
+ Plugin URI: https://github.com/blobaugh/2012-WordCamp-Orange-County-WP-Cron
+ Description: Code to go along with my WP-Cron presentation at the 2012 Orange County WordCamp conference 
+ Version: 0.1
+ Author: Ben Lobaugh
+ Author URI: http://ben.lobaugh.net
+ */
+
+
